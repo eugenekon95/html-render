d51c257e2725e822431b1b1567d73202ed98071d
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-11-17 14:29:34 +0200

    rubocop fixes

--- lib/pet_html.rb
+++ lib/pet_html.rb
@@ -1,21 +1,20 @@
+class PetHtml
+  def initialize(animal = "dog", name = "Black", health = 10, mood = 10, hunger = 10, sleeping = 10, stamina = 10, agility = 10, intelect = 10, purity = 10)
+    @animal = animal
+    @name = name
+    @health = health
+    @mood = mood
+    @hunger = hunger
+    @sleeping = sleeping
+    @stamina = stamina
+    @agility = agility
+    @intelect = intelect
+    @purity = purity
+  end
 
-    class PetHtml
-      def initialize(animal = "dog", name = "Black", health = 10, mood = 10, hunger = 10, sleeping = 10, stamina = 10, agility = 10, intelect = 10, purity = 10)
-          @animal = animal
-          @name = name
-          @health = health
-          @mood = mood
-          @hunger = hunger
-          @sleeping = sleeping
-          @stamina = stamina
-          @agility = agility
-          @intelect = intelect
-          @purity = purity
-        end
-
-    def makeHTML(filename = "index.html", content)
-      file = File.new(filename, "w+")
-      file.puts("<html>
+  def makeHTML(filename = "index.html", content)
+    file = File.new(filename, "w+")
+    file.puts("<html>
           <head>
             <meta charset='utf-8'>
             <title>Tamagotchi</title>
@@ -24,27 +23,27 @@
             <p>#{content}</p>
           </body>
         </html>")
-      file.close()
-      case content
-      when 'wash'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+    file.close()
+    case content
+    when 'wash'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <p>Purity = #{@purity += rand(6)}</p>
           <p> Mood = #{@mood += rand(6)}</p>
         </body>")
-        file.close()
-       when 'feed'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+      file.close()
+    when 'feed'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>You are feeding #{@name}</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger += rand(6)}</p>
         </body>")
-        file.close()
+      file.close()
 
-      when 'train'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+    when 'train'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>You are training #{@name}</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger -= rand(6)}</p>
@@ -53,38 +52,37 @@
           <p> Agility = #{@agility += rand(6)}</p>
           <p> Intelect = #{@intelect += rand(6)}</p>
         </body>")
-        file.close()
+      file.close()
 
-
-      when 'walk'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+    when 'walk'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>You are walking with #{@name}</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger -= rand(6)}</p>
           <p> Purity = #{@purity -= rand(6)}</p>
           <p> Mood = #{@mood += rand(6)}</p>
           <p> Stamina = #{@stamina -= rand(6)}</p>
-          
-        </body>")
-        file.close()
 
-      when 'play'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+        </body>")
+      file.close()
+
+    when 'play'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>You are playing with #{@name}</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger -= rand(6)}</p>
           <p> Sleeping = #{@sleeping -= rand(6)}</p>
           <p> Mood = #{@mood += rand(6)}</p>
           <p> Stamina = #{@stamina -= rand(6)}</p>
-          
-        </body>")
-        file.close()
 
-      when 'sleep'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+        </body>")
+      file.close()
+
+    when 'sleep'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>#{@name} is sleeping</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger -= rand(6)}</p>
@@ -92,25 +90,25 @@
           <p> Mood = #{@mood += rand(6)}</p>
           <p> Stamina = #{@stamina += rand(6)}</p>
           <p> Sleeping = #{@sleeping}</p>
-          
-        </body>")
-        file.close()
 
-      when 'search'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+        </body>")
+      file.close()
+
+    when 'search'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>#{@name} is searching</h2>
           <i> Characteristics after event </i>
           <p> Hunger = #{@hunger -= rand(6)}</p>
           <p> Mood = #{@mood += rand(6)}</p>
           <p> Stamina = #{@stamina -= rand(6)}</p>
-          <p> Intelect = #{@intelect += rand(6)}</p>  
+          <p> Intelect = #{@intelect += rand(6)}</p>
         </body>")
-        file.close()
-        
-      when 'status'
-        file = File.new(filename, "w+")
-        file.puts("<body>
+      file.close()
+
+    when 'status'
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <h2>#{@name} status</h2>
           <i> Characteristics</i>
           <p> Hunger = #{@hunger}</p>
@@ -120,21 +118,18 @@
           <p> Agility = #{@agility}</p>
           <p> Sleeping = #{@sleeping}</p>
         </body>")
-        file.close()
+      file.close()
 
-
-      else
-        file = File.new(filename, "w+")
-        file.puts("<body>
+    else
+      file = File.new(filename, "w+")
+      file.puts("<body>
           <p>Nothing to show</p>
         </body>")
-        file.close()
-      end
-      return file  
-      
-        
-    end    
+      file.close()
+    end
+    return file
   end
+end
 
 pet = PetHtml.new()
-pet.makeHTML( 'feed')
+pet.makeHTML('feed')
